09266ea4f48167ad9b306f34cc77ac9a9ae7fc7e
Ar-Ed <rased27273333@gmail.com>
2022-05-08 19:15:42 +0300

    optional output file name entry

--- main.py
+++ main.py
@@ -16,15 +16,26 @@ cb_save = tk.Checkbutton(text="Save", variable=is_save)
 cb_speak = tk.Checkbutton(text="Speak", variable=is_speak)
 cb_file = tk.Checkbutton(text="File Input", variable=is_file)
 
-# Entry and Button
-entry = tk.Entry()
+
+# Entries and Button
+input_label = tk.Label(text="Input")
+input = tk.Entry()
+
+output_label = tk.Label(text="Output File Name")
+output_name_entry = tk.Entry()
+
 button = tk.Button(text="Enter", 
-command=lambda: utils.handle_entry(entry.get(), is_save.get(), is_speak.get(), is_file.get()))
+command=lambda: utils.handle_entry(
+   input.get(), is_save.get(), is_speak.get(), is_file.get(), output_name_entry.get()))
+
 
 cb_file.pack()
 cb_save.pack()
 cb_speak.pack()
-entry.pack()
+input_label.pack()
+input.pack()
+output_label.pack()
+output_name_entry.pack()
 button.pack()
 
 window.mainloop()
--- utils.py
+++ utils.py
@@ -1,31 +1,36 @@
 import os
 import threading
+from datetime import datetime
 
 from gtts import gTTS
 from playsound import playsound
 
-FILE_NAME = "recording.mp3"
 
-def handle_entry(input, is_save, is_speak, is_file):
+def handle_entry(input, is_save, is_speak, is_file, output_name):
+   if not output_name:
+      # if output file name is not given use current date
+      output_name = datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
+   output_name = output_name + ".mp3"
+   
    if is_file:
-      _file_to_speech(input)
+      _file_to_speech(input, output_name)
    else: 
-      _text_to_speech(input)
+      _text_to_speech(input, output_name)
 
    if is_speak:
-      threading.Thread(target=lambda: _play_sound(FILE_NAME, is_save), daemon=True).start()
+      threading.Thread(target=lambda: _play_sound(output_name, is_save), daemon=True).start()
    
 def _play_sound(file_name, is_save):
-   playsound(file_name)       # play mp3
+   playsound(file_name)             # play mp3
    if not is_save:
-      os.remove(file_name)    # delete mp3
+      os.remove(file_name)          # delete mp3
 
 
-def _file_to_speech(file_name):
+def _file_to_speech(file_name, output_name):
    with open(file_name, "r") as file:
-      tts = gTTS(file.read()) # text -> speech
-      tts.save(FILE_NAME)     # save speech to mp3
+      tts = gTTS(file.read())       # text -> speech
+      tts.save(output_name)         # save speech to mp3
 
-def _text_to_speech(text):
-   tts = gTTS(text)           # text -> speech
-   tts.save(FILE_NAME)        # save speech to mp3
+def _text_to_speech(text, output_name):
+   tts = gTTS(text)                 # text -> speech
+   tts.save(output_name)            # save speech to mp3
