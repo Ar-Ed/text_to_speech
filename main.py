db48735813bc368e65457616df2757093a387fb2
Ar-Ed <rased27273333@gmail.com>
2022-05-08 18:29:45 +0300

    File input, save, speak control at the user level * GUI freezes when speaking (utilize multiple threads) * if save box is not checked it removes (add passive behaviour) * get user entry for the output file

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import tkinter as tk
 
-from utils import text_to_speech
+import utils
 
 # Create Window
 window = tk.Tk()
 window.title("text to speech")
 window.geometry("600x400")
 
-entry = tk.Entry()
-# call text_to_speech on button press
-button = tk.Button(text="Enter", command=lambda: text_to_speech(entry))
+# Checkbox and checkbox variables
+is_save = tk.BooleanVar()
+is_speak = tk.BooleanVar()
+is_file = tk.BooleanVar()
 
+cb_save = tk.Checkbutton(text="Save", variable=is_save)
+cb_speak = tk.Checkbutton(text="Speak", variable=is_speak)
+cb_file = tk.Checkbutton(text="File Input", variable=is_file)
+
+# Entry and Button
+entry = tk.Entry()
+button = tk.Button(text="Enter", 
+command=lambda: utils.handle_entry(entry.get(), is_save.get(), is_speak.get(), is_file.get()))
+
+cb_file.pack()
+cb_save.pack()
+cb_speak.pack()
 entry.pack()
 button.pack()
 
--- utils.py
+++ utils.py
@@ -3,12 +3,26 @@ import os
 from gtts import gTTS
 from playsound import playsound
 
-
 FILE_NAME = "recording.mp3"
 
-def text_to_speech(entry):
-   tts = gTTS(entry.get()) # text -> speech
-   tts.save(FILE_NAME)     # save speech to mp3
+def handle_entry(input, is_save, is_speak, is_file):
+   if is_file:
+      _file_to_speech(input)
+   else: 
+      _text_to_speech(input)
 
-   playsound(FILE_NAME)    # play mp3
-   os.remove(FILE_NAME)    # delete mp3
+   if is_speak:
+      playsound(FILE_NAME)    # play mp3
+
+   if not is_save:
+      os.remove(FILE_NAME)    # delete mp3
+   
+
+def _file_to_speech(file_name):
+   with open(file_name, "r") as file:
+      tts = gTTS(file.read()) # text -> speech
+      tts.save(FILE_NAME)     # save speech to mp3
+
+def _text_to_speech(text):
+   tts = gTTS(text)           # text -> speech
+   tts.save(FILE_NAME)        # save speech to mp3
