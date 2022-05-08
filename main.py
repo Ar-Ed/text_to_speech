b1564d8bc352f732d8329b438befe7d15a182490
Ar-Ed <rased27273333@gmail.com>
2022-05-08 19:59:37 +0300

    All the languages supported by gTTS are useable by specifying via a dropdown menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 import utils
 
@@ -16,6 +17,10 @@ cb_save = tk.Checkbutton(text="Save", variable=is_save)
 cb_speak = tk.Checkbutton(text="Speak", variable=is_speak)
 cb_file = tk.Checkbutton(text="File Input", variable=is_file)
 
+# Language selection
+lang_var = tk.StringVar()
+lang_list = ttk.Combobox(textvariable=lang_var, values=tuple(utils.languages))
+lang_list.current(tuple(utils.languages).index("en"))
 
 # Entries and Button
 input_label = tk.Label(text="Input")
@@ -26,7 +31,7 @@ output_name_entry = tk.Entry()
 
 button = tk.Button(text="Enter", 
 command=lambda: utils.handle_entry(
-   input.get(), is_save.get(), is_speak.get(), is_file.get(), output_name_entry.get()))
+   input.get(), is_save.get(), is_speak.get(), is_file.get(), output_name_entry.get(), lang_var.get()))
 
 
 cb_file.pack()
@@ -37,5 +42,6 @@ input.pack()
 output_label.pack()
 output_name_entry.pack()
 button.pack()
+lang_list.pack()
 
 window.mainloop()
--- utils.py
+++ utils.py
@@ -2,20 +2,23 @@ import os
 import threading
 from datetime import datetime
 
-from gtts import gTTS
+from gtts import gTTS, lang
 from playsound import playsound
 
+# supported languages
+languages = lang.tts_langs().keys()
 
-def handle_entry(input, is_save, is_speak, is_file, output_name):
+
+def handle_entry(input, is_save, is_speak, is_file, output_name, lang):
    if not output_name:
       # if output file name is not given use current date
       output_name = datetime.now().strftime("%H_%M_%S_%d_%m_%Y")
    output_name = output_name + ".mp3"
    
    if is_file:
-      _file_to_speech(input, output_name)
+      _file_to_speech(input, output_name, lang)
    else: 
-      _text_to_speech(input, output_name)
+      _text_to_speech(input, output_name, lang)
 
    if is_speak:
       threading.Thread(target=lambda: _play_sound(output_name, is_save), daemon=True).start()
@@ -26,11 +29,11 @@ def _play_sound(file_name, is_save):
       os.remove(file_name)          # delete mp3
 
 
-def _file_to_speech(file_name, output_name):
+def _file_to_speech(file_name, output_name, lang):
    with open(file_name, "r") as file:
-      tts = gTTS(file.read())       # text -> speech
+      tts = gTTS(file.read(), lang=lang)       # text -> speech
       tts.save(output_name)         # save speech to mp3
 
-def _text_to_speech(text, output_name):
+def _text_to_speech(text, output_name, lang):
    tts = gTTS(text)                 # text -> speech
    tts.save(output_name)            # save speech to mp3
